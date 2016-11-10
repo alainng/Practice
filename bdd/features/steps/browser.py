5aa2c11ed08196410613c419e7d150f828479805
Alain Ng <alain.ng@wajam.com>
2016-11-10 18:09:47 -0500

    working chrome prototype

--- bdd/features/steps/browser.py
+++ bdd/features/steps/browser.py
@@ -2,27 +2,31 @@ from behave import *
 from selenium import webdriver
 from nose.tools import assert_raises, assert_true, assert_false
 
-#_browser = None
+_browser = None
 
 def open_browser(name):
+    global _browser
     try:
         if name == "Chrome":
-            _browser = webdriver.Chrome(executable_path="C:\\tests\bdd\selenium_drivers\\chromedriver.exe", service_args=["--verbose", "--log-path=c:\\chromedriver.log"])
-        _browser.set_page_load_timeout(60) #deadcode?
+            _browser = webdriver.Chrome(executable_path="C:\\tests\\selenium_drivers\\chromedriver.exe")
     except Exception as e:
         print("Unexpected error: {}".format(repr(e)))
-    return _browser
 
+def visit_url(url):
+    try:
+        _browser.get(url)
+    except Exception as e:
+        print("Unexpected error: {}".format(repr(e)))
+        
 @given("Chrome is installed")
 def is_chrome_installed(context):
-    browser=open_browser("Chrome")
-    browser.geturl("https://www.google.com")
+    open_browser("Chrome")
+    visit_url("https://www.google.com")
     return True
 
 @when("I launch Chrome")
 def launch_browser(context):
-    
-    return True
+    open_browser("Chrome")
     
 
 @then("Chrome is open")
