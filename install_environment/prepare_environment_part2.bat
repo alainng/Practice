c15d9aa703946ecd8d55b4877e1557ef4e30a8ef
Alain Ng <alain.ng@wajam.com>
2016-11-11 10:36:49 -0500

    More comprehensive list of dependencies

--- install_environment/prepare_environment_part2.bat
+++ install_environment/prepare_environment_part2.bat
@@ -1,8 +1,24 @@
 echo pip installing dependencies
-pip install --upgrade selenium
+::Run behavior driven tests
 pip install --upgrade behave
+
+::control browser actions
+pip install --upgrade selenium
+
+::Screenshot actions
 pip install --upgrade pyscreenshot
+
+::Test functions (Asserts, etc)
 pip install --upgrade nose
+
+::Headless download
+::pip install --upgrade wget
+
+::pywin32 wrapper to simplify implementation details
+pip install --upgrade wmi
+
+::process and system utilization
+pip install --upgrade psutil
 
 echo Done pip installing
 pause
