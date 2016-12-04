a38dc75cc785cf4e326be8f4e1a4b8eb34b3bb72
Milwy <Milwy@Zikora>
2016-12-04 18:59:36 -0500

    More interview questions

---
+++ PalindromeChecker.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+def isPalindrome(inputString):
+	n=len(inputString)
+	for i in range(0,n/2):
+		if inputString[i] != inputString[n-i-1]:
+			return False
+	return True
+
+print isPalindrome("T1o1T")
