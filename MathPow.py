5a647cfcebbba2734ee8c708a6502636d7f0612a
Milwy <Milwy@Zikora>
2016-12-04 19:26:56 -0500

    Added usable tests

--- MathPow.py
+++ MathPow.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from __future__ import division	#Because python 2.7 division sucks
+import pytest
 
 def pow(base,exponent):
 	result=1.0
@@ -9,4 +10,9 @@ def pow(base,exponent):
 		else:
 			result=result/base
 	return result
-	
+
+def test_pow():
+	assert pow(2,2)==4.0
+	assert pow(10,-2)==0.01
+	assert pow(1000,1)==1000.0
+	assert pow(1000,0)==1.0
