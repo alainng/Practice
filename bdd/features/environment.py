8080267e128b4f114caca322e2d5ffd8394341de
Alain Ng <alain.ng@wajam.com>
2016-12-08 16:48:15 -0500

    template functions added to environment.py

--- bdd/features/environment.py
+++ bdd/features/environment.py
@@ -1,1 +1,12 @@
-print ("This is env file")
+# def before_all(context):
+#     return True
+# 
+# def before_scenario(context,scenario):
+#     if 'abc' in scenario.tags:
+#         return True
+
+# def after_scenario(context,scenario):
+#     return True
+# 
+# def after_all(context):
+#     return True
