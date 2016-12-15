4bff01abff191cebe72f3c583c48880fbcac20ad
Alain Ng <alain.ng@wajam.com>
2016-12-15 15:08:26 -0500

    reference page

--- bdd/features/steps/browser.py
+++ bdd/features/steps/browser.py
@@ -1,26 +1,8 @@
 from behave import *
 from features.lib.pages import *
 from features.lib.driver import *
-# from selenium.common.exceptions import WebDriverException
-# from selenium import webdriver
-# 
-# class DriverFactory:
-#     def getDriver(self,input_name):    
-#         name=input_name.lower()
-#         try:
-#             if name == "ie":
-#                 return webdriver.Ie(executable_path=r"C:\selenium_drivers\IEDriverServer.exe")
-#             elif name == "edge":
-#                 return webdriver.Edge(executable_path=r"C:\selenium_drivers\MicrosoftWebDriver.exe")
-#             elif name == "firefox":            
-#                 return webdriver.Firefox(executable_path=r"C:\selenium_drivers\geckodriver.exe")          
-#             else:
-#                 return webdriver.Chrome(executable_path=r"C:\selenium_drivers\chromedriver.exe")
-#         except WebDriverException as wde:
-#             print("{} did not open as expected".format(wde))
-#             raise
-#         except Exception as e:
-#             print("Unexpected error: {}".format(repr(e)))
+
+#https://github.com/machzqcq/Python_Page_Object/blob/master/features/lib/pages/order_confirmation_page.py
 
 def create_driver(context,browser):
     drv = getattr(context, "driver", None)
